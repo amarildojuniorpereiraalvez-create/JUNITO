38aa0c80a3d6978ea090f505d1c942d5f6eee895
amarildojuniorpereiraalvez-create <amarildojuniorpereiraalvez@gmail.com>
2026-06-15 00:24:58 -0300

    loader.lua

--- loader.lua
+++ loader.lua
@@ -1,18 +1,17 @@
-if not game() then
-game.Loaded()
+if not game:IsLoaded() then
+game.Loaded:Wait()
 end
 
-local BASE_URL = "https://raw.githubusercontent.com/amarildojuniorpereiraalvez-create/JUNITO/refs/heads/main"
-local SCRIPT_FILE = "animeastral.lua"
+local ORIGINAL_URL = "https://raw.githubusercontent.com/ZhangJunZ84/twvzyyds/refs/heads/main/animeastral.lua"
 
-local url = BASE_URL .. "/" .. SCRIPT_FILE
-
-print("[JR] Carregando:", url)
+print("[JR] Carregando Anime Astral original limpo...")
 
 local ok, err = pcall(function()
-loadstring(game(url))()
+loadstring(game:HttpGet(ORIGINAL_URL))()
 end)
 
 if not ok then
-warn("[JR] Erro ao carregar " .. SCRIPT_FILE .. ": " .. tostring(err))
+warn("[JR] Erro ao carregar Anime Astral original: " .. tostring(err))
+else
+print("[JR] Anime Astral original carregado com sucesso.")
 end
